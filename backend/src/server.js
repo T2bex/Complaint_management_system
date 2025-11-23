664dc4596c2c522cf070206f4a18a9c6c8534440
T2bex <tobechukwu14@yahoo.com>
2025-11-23 18:50:30 +0000

    updated server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,10 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.get('/hello', (req, res) => {
-    req.body
-    res.send('Hello');
-});
+mongoose.connect("mongodb://127.0.0.1:27017/Complaint_system")
 
 app.listen(8000, () => {
-    console.log('Server is listening on port 8000!')
+    console.log("server is running")
 })
